52524034380c97c584eeb52db6f49d6919cdcdd6
Zach David <112095798+Zxayleral@users.noreply.github.com>
2024-02-28 23:21:51 +0800

    changed some functions

--- src/app/config/utils.py
+++ src/app/config/utils.py
@@ -3,21 +3,42 @@ import os
 
 
 class Parser:
-	def __init__(self):
-		self.filename: str
-		self.header: list
+	def __init__(self, filename, header):
+		self.filename = filename
+		self.header = header
+		self.initialize()
 
 	def initialize(self):
+		if os.path.exists(self.filename):
+			existing_data = []
+			with open(self.filename, 'r') as file:
+				reader = csv.DictReader(file)
+				existing_data = [row for row in reader]
+			if not existing_data:
+				with open(self.filename, 'w', newline='') as file:
+					writer = csv.DictWriter(file, fieldnames = self.header)
+					writer.writeheader()
+			return True
+
 		if not os.path.exists(self.filename):
-			with open(self.filename, "w") as file:
+			with open(self.filename, "w", newline='') as file:
 				writer = csv.DictWriter(file, fieldnames=self.header)
+				writer.writeheader()
 
 	def count(self):
+		self.initalize()
 		with open(self.filename, "r") as file:
 			reader = csv.DictReader(file)
 			return len(list(x for x in reader))
 
+	def insert_one(self, document: dict):
+		self.initialize()
+		with open(self.filename, "a", newline='') as file:
+			writer = csv.DictWriter(file, fieldnames=self.header)
+			writer.writerow(document)
+
 	def check(self, document):
+		self.initialize()
 		collections = self.read_all()
 		for collection in collections:
 			if collection["id"] == document["id"]:
@@ -25,11 +46,13 @@ class Parser:
 		return False
 
 	def read_all(self):
+		self.initialize()
 		with open(self.filename, "r") as file:
 			reader = csv.DictReader(file)
 			return list(x for x in reader)
 
 	def delete_one(self, document):
+		self.initialize()
 		if not self.check(document):
 			return False
 		collections = self.read_all()
@@ -40,4 +63,13 @@ class Parser:
 					continue
 				else:
 					writer.writerow(student)
-		
+
+student = Parser(
+	filename="./data/csv/students.csv", 
+	header=["id", "name", "gender", "course"]
+)
+
+course = Parser(
+	filename="./data/csv/courses.csv",
+	header=["id", "name", "description"]
+)
--- src/app/frames/add_student.py
+++ src/app/frames/add_student.py
@@ -12,32 +12,17 @@ class AddStudent(tk.Frame):
 			width=600,
 			height=500
 		)
-		self.student_id = None
-		self.student_name = None
-		self.student_gender = None
-
 		# ADD COURSE LABEL
-		self.add_course_label = tk.Label(self, text="Add Student", 
-									 	 font=('Default', 20), bg=color.PEACH, fg=color.CHARCOAL)
-		self.add_course_label.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
-
-		self.id_label = tk.Label(self, text="ID: ", font=('Default', 13), 
-								 bg=color.PEACH, fg=color.CHARCOAL).place(relx=0.3, rely=0.4, anchor=tk.CENTER)
-		self.id_entry = tk.Entry(self, width=18, bg=color.PEACH, 
-								 fg=color.CHARCOAL, font=('Default', 13))
-		self.id_entry.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
-
-		self.name_label = tk.Label(self, text="Name:", font=('Default', 13), 
-								   bg=color.PEACH, fg=color.CHARCOAL).place(relx=0.265, rely=0.5, anchor=tk.CENTER)
-		self.name_entry = tk.Entry(self, width=18, bg=color.PEACH, 
-								   fg=color.CHARCOAL, font=('Default', 13))
-		self.name_entry.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-
-		self.gender_label = tk.Label(self, text="Gender:", font=('Default', 13),
-										  bg=color.PEACH, fg=color.CHARCOAL).place(relx=0.254, rely=0.6, anchor=tk.CENTER)
-
-		self.gender_entry = ttk.Combobox(self, 
-			width=18, font=('Default', 13))
+		self.add_course_label = tk.Label(self, text="Add Student", font=('Default', 20), bg=color.PEACH, fg=color.CHARCOAL)
+		self.add_course_label.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+		self.id_label = tk.Label(self, text="ID: ", font=('Default', 13), bg=color.PEACH, fg=color.CHARCOAL).place(relx=0.3, rely=0.4, anchor=tk.CENTER)
+		self.id_entry = tk.Entry(self, width=18, bg=color.PEACH, fg=color.CHARCOAL, font=('Default', 13))
+		self.id_entry.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+		self.name_label = tk.Label(self, text="Name:", font=('Default', 13), bg=color.PEACH, fg=color.CHARCOAL).place(relx=0.265, rely=0.3, anchor=tk.CENTER)
+		self.name_entry = tk.Entry(self, width=18, bg=color.PEACH, fg=color.CHARCOAL, font=('Default', 13))
+		self.name_entry.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+		self.gender_label = tk.Label(self, text="Gender:", font=('Default', 13), bg=color.PEACH, fg=color.CHARCOAL).place(relx=0.254, rely=0.5, anchor=tk.CENTER)
+		self.gender_entry = ttk.Combobox(self, width=17, font=('Default', 13))
 		self.gender_entry["values"] = [
 			"Male", "Female", "Male (Born Female)", "Female (Born Male)",
 			"Croissant", "Attack Helicopter", "Trans Racist", "Male (Born Alone)",
@@ -45,10 +30,10 @@ class AddStudent(tk.Frame):
 			"Female (Filipino)", "Female (Afam Enjoyer)", "Male (Alpha)", "Sigma", "Chill Bastard"
 		]
 		self.gender_entry.current()
+		self.gender_entry.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 
-		self.gender_entry.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
-
+		# buttons
 		self.clear_button = tk.Button(
 			self,
 			text="CLEAR",
@@ -73,6 +58,7 @@ class AddStudent(tk.Frame):
 
 	def add_button_callback(self):
 		pass
+		
 
 	def clear_button_callback(self):
 		self.id_entry.delete(0, tk.END)
