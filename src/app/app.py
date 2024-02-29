4b03155db935da88696a3d29351e358cef2380e7
Zach David <112095798+Zxayleral@users.noreply.github.com>
2024-02-29 20:38:56 +0800

    Update app.py

--- src/app/app.py
+++ src/app/app.py
@@ -13,6 +13,7 @@ class LeftFrameButton(tk.Button):
 			height=1,
 			bg=color.BLUE,
 			fg=color.WHITE,
+			font=("Default", 13),
 			relief='groove',
 			borderwidth=1,
 			command=command
@@ -28,10 +29,10 @@ class LeftFrame(tk.Frame):
 		)
 		self.master = master
 		# construct the message enum
-		self.message = Message()
 		self.title()
 		self.buttons()
 		self.place_buttons()
+		self.current_frame = None
 
 	def title(self):
 		title1 = tk.Label(self, text="Simple Student\nInformation System", fg=color.WHITE, bg=color.BLUE)
@@ -41,7 +42,7 @@ class LeftFrame(tk.Frame):
 		self.add_course = LeftFrameButton(self, text="Add Course", command=self.master.add_course_button)
 		self.add_student = LeftFrameButton(self, text="Add Student", command=self.master.add_student_button)
 		self.list_courses = LeftFrameButton(self, text="List Courses", command=self.master.list_courses_button)
-		self.list_students = LeftFrameButton(self, text="List Students", command=self.list_students_panel)
+		self.list_students = LeftFrameButton(self, text="List Students", command=self.master.list_students_button)
 		self.quit_button = LeftFrameButton(
 			self, 
 			text="Quit",
@@ -56,46 +57,55 @@ class LeftFrame(tk.Frame):
 		self.list_students.place(relx=0.5, rely=0.54, anchor=tk.CENTER)
 		self.quit_button.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
 
-# Message Enum, used for states.
-class Message:
-	def __init__(self):
-		self.add_course = False
-		self.add_student = False
-		self.list_courses = False 
-		self.list_students = False
 
 
 class StudentSystem(tk.Tk):
 	def __init__(self):
 		super().__init__()
-		self.title = "Student System"
 		self.geometry("800x500")
 		self.resizable(False, False)
 		self.configure(
 			bg=color.PEACH
 		)
-
+		self.title("Simple Student Information System")
 
 		self.left_panel = LeftFrame(self)
 		self.left_panel.place(x=0, y=0)
+
+		self.current_frame = None
 		self.load_main_frame()
 
+	def destroy_last_frame(self):
+		if self.current_frame is None:
+			return
+		self.current_frame.destroy()
+
 	def load_main_frame(self):
+		self.destroy_last_frame()
 		self.starter_panel = MainPanel(self)
 		self.starter_panel.place(x=200, y=0)
+		self.current_frame = self.starter_panel
 
 	def add_course_button(self):
+		self.destroy_last_frame()
 		self.add_course_panel = AddCourse(self)
 		self.add_course_panel.place(x=200, y=0)
+		self.current_frame = self.add_course_panel
 
 	def add_student_button(self):
+		self.destroy_last_frame()
 		self.add_student_panel = AddStudent(self)
 		self.add_student_panel.place(x=200, y=0)
+		self.current_frame = self.add_student_panel
 
 	def list_courses_button(self):
+		self.destroy_last_frame()
 		self.list_courses_panel = ListCourses(self)
 		self.list_courses_panel.place(x=200, y=0)
+		self.current_frame = self.list_courses_panel
 
 	def list_students_button(self):
+		self.destroy_last_frame()
 		self.list_students_panel = ListStudents(self)
 		self.list_students_panel.place(x=200, y=0)
+		self.current_frame = self.list_students_panel
